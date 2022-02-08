7552b3a9df772f4040abc7c8b5425092ff07e001
ItaloCovas <italocovas@gmail.com>
2022-02-08 15:12:08 -0300

    Tela de finalizado

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 const primaryGrey = Color(0xFF767676);
-const secondaryGray = Color(0xFFC4C4C4);
+const secondaryGray = Color(0xFFECECEC);
 const primaryGreen = Color(0XFF48A865);
 const textColor = Color(0xFFFFFfff);
